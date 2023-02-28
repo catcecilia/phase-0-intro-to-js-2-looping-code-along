3731290e9e40b41dc6d225a9f1b82d241c665cff
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-02-28 13:02:01 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, typeOfGift){
+    let newArray = [];
+    for (let counter = 0; counter < array.length; counter++) {
+        newArray.push(`Thank you, ${array[counter]}, for the wonderful ${typeOfGift} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
